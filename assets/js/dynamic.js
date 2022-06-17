14ebb189bf8b382eae945ee87e09c0990f621269
Kervyn <kervyntan@gmail.com>
2022-06-18 01:16:19 +0800

    catch the response from the fetch using res, changed the text of each item to data in json

--- assets/js/dynamic.js
+++ assets/js/dynamic.js
@@ -1,1 +1,22 @@
 // fetch the data from the local json file
+// calculate the total amount from the json, then sum it up
+// take each amount, make it a percentage of the total
+// make that percentage the height of each day
+const fetchDailyExpenditure = async () => {
+
+    // fetch("../../data.json")
+    // .then(response => response.json())
+    // .then(data => catchData(data))
+    let res = await(fetch("../../data.json").then(
+        response => response.json()
+    ))
+
+    // console.log(res)
+    const listOfDays = document.querySelectorAll('.bar-chart-list-item')
+    console.log(listOfDays)
+    for (let i = 0; i < listOfDays.length; i++) {
+        listOfDays[i].children[1].innerText = res[i].day;
+    }
+}
+
+fetchDailyExpenditure();
