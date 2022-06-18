aee35baf8bd346a7820dd46cccf552a3cd32d433
Kervyn <kervyntan@gmail.com>
2022-06-18 11:09:38 +0800

    added hover show and disappear class

--- assets/js/dynamic.js
+++ assets/js/dynamic.js
@@ -7,18 +7,40 @@ const fetchDailyExpenditure = async () => {
     // fetch("../../data.json")
     // .then(response => response.json())
     // .then(data => catchData(data))
+    
+    // fetch the data, then parses the JSON into JS object
     let res = await(fetch("../../data.json").then(
         response => response.json()
     ))
 
-    // console.log(res)
     const listOfDays = document.querySelectorAll('.bar-chart-list-item')
-    console.log(listOfDays)
+    const listOfBarAmount = document.querySelectorAll('.bar-amount')
+    // console.log(listOfDays)
+
+    const targetParaElement = 2;
     for (let i = 0; i < listOfDays.length; i++) {
-        listOfDays[i].children[1].innerText = res[i].day;
+        // ensure that each list item in the bar chart represents the specific day
+        listOfDays[i].children[targetParaElement].innerText = res[i].day;
         // listOfDays[i].children[0].style.height = res[i].amount + "px";
+
+        // documentElement -> css inside :root
+        // setting each individual animation property
+        listOfBarAmount[i].innerText = '$' + res[i].amount;
         document.documentElement.style.setProperty('--animation-end-width-' + res[i].day, res[i].amount + "px");
+
+        listOfDays[i].addEventListener('mouseover', () => {
+            const barAmount = listOfBarAmount[i];
+            barAmount.classList.add("show");
+            barAmount.classList.remove("disappear");
+        })
+
+        listOfDays[i].addEventListener('mouseleave', () => {
+            const barAmount = listOfBarAmount[i];
+            barAmount.classList.add("disappear");
+            barAmount.classList.remove("show");
+        })
     }
 }
+
 
 fetchDailyExpenditure();
